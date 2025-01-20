efaa706c89b8a40649e59a7dd5f15eccfb68c522
manoharreddyparne <parnemanoharreddy19@gmail.com>
2025-01-20 11:41:49 +0530

    Day-10

--- arrays.js
+++ arrays.js
@@ -1,4 +1,23 @@
 
 const users = ["Manohar Reddy","Yadav","patel","doremon"];
+console.log(users);
 let x = users.join("")
 console.log(x,typeof(x));
+
+//arrow method
+users.map((user, i)=>{
+    console.log(user,i);
+
+})
+// storing and calling the function
+let y = users.map((user,i)=>{
+    return user;
+})
+console.log(y);
+
+console.log("From here is the forEach method")
+let z = users.forEach((user,i)=>{
+    console.log(user);
+    return user;  // returns an undefined
+})
+console.log(z);
--- objects.js
+++ objects.js
@@ -4,3 +4,35 @@ let userdetails = {
 }
 console.log(userdetails);
 console.log(userdetails.age);
+
+//neseted objects:
+let details = {
+    name:"UB1",
+    age:23,
+    pincode:1312,
+    mobile:{
+        mb1:709231124,
+        mb2:12124124
+    }
+
+}
+console.log(details);
+console.log(details.name);
+console.log(details.mobile.mb1);
+// objects inside the array
+
+let users = [
+    {
+        name:"Manohar",
+        age:21
+    },{
+    name:"Reddy",
+    age:19
+    },
+    {
+        name:"Yadav",
+        age:20
+    }
+]
+console.log(users);
+console.log(users.name)
