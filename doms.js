086cea8aa30b2c90b261275a212212b1822c08fc
manoharreddyparne <parnemanoharreddy19@gmail.com>
2025-01-24 11:39:59 +0530

    DAY-14

--- doms.js
+++ doms.js
@@ -1,2 +1,31 @@
-let element = document.getElementById("demo");
-// console.log(element);
+//using id method
+let element1 = document.getElementById("demo");
+console.log(element1);
+
+
+//using class method
+let ele = document.getElementsByClassName("test");
+// ele[0].style.backgroundColor="dodgerblue"; //this allow to style the element using index.
+// console.log(ele);
+// console.log(Array.isArray(ele)); // This method is used to return boolean values based on whether the array is pure array or not.
+
+
+//! spread operator
+let x = [...ele];
+console.log(x,Array.isArray(x));
+x.map(element=>{
+    element.style.backgroundColor="tomato";
+    element.style.color="white";
+    element.style.textTransform="uppercase"; //used to change the case.
+});
+
+
+//query selector
+let ele1=document.querySelector("#demo");
+console.log(ele1);
+let ele2 = document.querySelector(".test");
+console.log(ele2);
+
+//query selectorAll
+let ele3 = document.querySelectorAll(".test");
+console.log(ele3);
